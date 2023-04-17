a7ce558204099921d4626ddc93ceab72cdab0d56
Kim Jones <qawithkimj@gmail.com>
2023-04-16 21:26:14 -0500

    latest commit as of april 16 2023

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://0e3b9a2f-1854-4a6a-98e3-4f5afb1686d1.serverhub.practicum-services.com"
+    API_URL: "https://fc56ca6f-88f3-437e-9cf1-c02bcca3195d.serverhub.practicum-services.com"
 }
